7c74e216506cabd429d80e8b235f4812fe900c2f
pexel <119343660+Daniil-Dippel@users.noreply.github.com>
2025-07-10 20:09:05 +0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 app = Flask(__name__)
 CORS(app)
 
-HF_TOKEN = "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"
+HF_TOKEN = "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"
 MODEL = "tiiuae/falcon-7b-instruct"  # стабильная модель
 
 @app.route('/api/chat', methods=['POST'])
